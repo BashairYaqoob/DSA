5ba7158a2c917b97808e226cdf1e3aa86f476c1e
bashair yaqoob <bashairyaqoob@gmail.com>
2025-08-27 21:43:48 +0500

    Update q4.cpp

--- lab2/q4.cpp
+++ lab2/q4.cpp
@@ -1,19 +1,18 @@
+//jagged array
 #include <iostream>
 using namespace std;
 
 int main(){
 	int dep=4;
 	string department[4]={"SE", "AI", "CS", "DS"};
-	int **arr= new int*[dep];
+	float **arr= new float*[dep];
 	int size[4];
 	
 	for(int i=0; i<dep; i++){
 		cout<<"Enter the number of coure courses for "<<department[i]<<": ";
 		cin>>size[i];
-		arr[i]= new int[size[i]];
-	}
-	for(int i=0; i<dep;i++){
-		for(int j=0; j<size[i]; j++){
+		arr[i]= new float[size[i]];
+        for(int j=0; j<size[i]; j++){
 			cout<<"Enter GPA for course "<< j+1<<" in "<<department[i]<<": ";
 			cin>>arr[i][j];
 		}
@@ -21,9 +20,9 @@ int main(){
 	
 	for(int i=0; i<dep; i++){
 		cout<<"Department: "<<department[i]<<endl;
-		cout<<"Courses GPAs:\n";
+		cout<<"Courses GPAs: ";
 		for(int j=0;j<size[i];j++){
-			cout<<"Course "<<j+1<<arr[i][j]<<endl;
+			cout<<"\nCourse "<<j+1<<": "<<arr[i][j];
 		}
 		cout<<endl;
 	}
