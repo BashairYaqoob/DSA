bc0ee853e063101e0566761460290101c9a6e0e2
bashair yaqoob <bashairyaqoob@gmail.com>
2025-08-27 10:27:05 +0500

    Update q4.cpp

--- lab2/q4.cpp
+++ lab2/q4.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
 	int dep=4;
-	string deparment[4]={"SE", "AI", "CS", "DS"};
+	string department[4]={"SE", "AI", "CS", "DS"};
 	int **arr= new int*[dep];
 	int size[4];
 	
 	for(int i=0; i<dep; i++){
 		cout<<"Enter the number of coure courses for "<<department[i]<<": ";
-		cin<<size[i];
+		cin>>size[i];
 		arr[i]= new int[size[i]];
 	}
 	for(int i=0; i<dep;i++){
@@ -21,7 +21,7 @@ int main(){
 	
 	for(int i=0; i<dep; i++){
 		cout<<"Department: "<<department[i]<<endl;
-		cout<<"Courses GPAs:\n"
+		cout<<"Courses GPAs:\n";
 		for(int j=0;j<size[i];j++){
 			cout<<"Course "<<j+1<<arr[i][j]<<endl;
 		}
