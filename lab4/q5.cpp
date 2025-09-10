8d8e3eb482ab88795877f51b3201762329c4ac53
bashair yaqoob <bashairyaqoob@gmail.com>
2025-09-10 13:53:27 +0500

    Create q5.cpp

---
+++ lab4/q5.cpp
@@ -0,0 +1,32 @@
+//Insertion sort
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myarr[]={9,5,1,4,3,14,119};
+    int n=sizeof(myarr) / sizeof(myarr[0]);
+
+    cout<<"Input myarr[]:\n";
+    for(int i=0; i<n; i++){
+        cout<<myarr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=1; i<n; i++){
+        int key=myarr[i];
+        int j=i-1;
+        while(j>=0 && myarr[j]>key){
+            myarr[j+1]=myarr[j];
+            j--;
+        }
+        myarr[j+1]=key;
+    }
+
+    cout<<"Insertion sort of myarr[]:\n";
+    for(int i=0; i<n; i++){
+        cout<<myarr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
